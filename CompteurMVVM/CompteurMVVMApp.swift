c7221a148252c871831fed71a2aa7908543515dc
zcsami <zcsami@gmail.com>
2024-12-24 09:17:20 +0100

    Initial Commit

---
+++ CompteurMVVM/CompteurMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompteurMVVMApp.swift
+//  CompteurMVVM
+//
+//  Created by Sami on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CompteurMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
